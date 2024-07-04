c35716f21ec65f5e1b6c451eb563061354617a5e
oshrag <oshra.greenblat@gmail.com>
2024-07-05 00:37:15 +0300

    add remove station, improve StationList view

--- src/cmps/StationList.jsx
+++ src/cmps/StationList.jsx
@@ -1,27 +1,33 @@
-import { useSelector } from "react-redux"
-import { StationPreview } from "./StationPreview"
-import { useEffect } from "react"
-import { loadStations } from "../store/actions/station.actions"
-import { getLoggedOnUser } from "../store/actions/user.actions"
+import { useSelector } from "react-redux";
+import { StationPreview } from "./StationPreview";
+import { useEffect } from "react";
+import { loadStations } from "../store/actions/station.actions";
+import { getLoggedOnUser } from "../store/actions/user.actions";
 
 // Checked - All looks good.
 
 export const StationList = ({ location }) => {
-    const stations = useSelector(storeState => storeState.stationModule.stations)
+  const stations = useSelector(
+    (storeState) => storeState.stationModule.stations
+  );
 
-    useEffect(() => {
-        loadStations()
-    }, [])
+  useEffect(() => {
+    loadStations();
+  }, []);
 
-    if (!stations) return
-    return (
-        <>
-            {location === "main" && <h3>Made for {getLoggedOnUser().name}</h3>}
-            <ul className="station-list">
-                {stations.map(station =>
-                    <StationPreview key={station._id} station={station} location={location} />
-                )}
-            </ul>
-        </>
-    )
-}
+  if (!stations) return;
+  return (
+    <>
+      {location === "main" && <h3>Made for {getLoggedOnUser().name}</h3>}
+      <ul className={`station-list ${location}`}>
+        {stations.map((station) => (
+          <StationPreview
+            key={station._id}
+            station={station}
+            location={location}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
