4d7614b297faf8ccd4170c70fb09afcdfce30e04
eitany52 <eitanpowerful@gmail.com>
2024-07-10 14:21:57 +0300

    improve search result style

--- src/cmps/SearchResult.jsx
+++ src/cmps/SearchResult.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import {
-  addSongToStation,
-  getSongsFromYoutube,
-  loadLikedSongsStation,
+    addSongToStation,
+    getSongsFromYoutube,
+    loadLikedSongsStation,
 } from "../store/actions/station.actions";
 import { useSelector } from "react-redux";
 import { useEffectUpdate } from "../customHooks/useEffectUpdate";
@@ -12,45 +12,48 @@ import { SongList } from "./SongList";
 //Checked - All looks good.
 
 export const SearchResult = () => {
-  const params = useParams();
-  const [songs, setSongs] = useState(null);
-  const likedSongsStation = useSelector(
-    (storeState) => storeState.stationModule.station
-  );
+    const params = useParams();
+    const [songs, setSongs] = useState(null);
+    const likedSongsStation = useSelector(
+        (storeState) => storeState.stationModule.station
+    );
 
-  useEffect(() => {
-    loadLikedSongsStation();
-  }, []);
+    useEffect(() => {
+        loadLikedSongsStation();
+    }, []);
 
-  useEffectUpdate(() => {
-    loadSongs();
-  }, [params]);
+    useEffectUpdate(() => {
+        loadSongs();
+    }, [params]);
 
-  function loadSongs() {
-    setSongs(getSongsFromYoutube());
-  }
-
-  function isSongSavedAtLikedSongs(song) {
-    return likedSongsStation.songs.some((_song) => _song.id === song.id);
-  }
-
-  async function onAddToLikedSongs(songToAdd) {
-    try {
-      await addSongToStation(likedSongsStation._id, songToAdd);
-      loadLikedSongsStation();
-    } catch (error) {
-      console.log("Having issues with saving this song");
+    function loadSongs() {
+        setSongs(getSongsFromYoutube());
     }
-  }
 
-  function onMoreOptions() {}
+    function isSongSavedAtLikedSongs(song) {
+        return likedSongsStation.songs.some((_song) => _song.id === song.id);
+    }
+
+    async function onAddToLikedSongs(songToAdd) {
+        try {
+            await addSongToStation(likedSongsStation._id, songToAdd);
+            loadLikedSongsStation();
+        } catch (error) {
+            console.log("Having issues with saving this song");
+        }
+    }
+
+    function onMoreOptions() { }
 
     console.log("SearchResult rendered");
+    console.log("params:", params);
     return (
         <div className="search-result">
             {/* <RecentSearches/> */}
             {params.userInput && songs && (
-                <section className="search-result-container">
+                <>
+
+                    {/* <section className="search-result-container">
                     <div className="btns-container">
                         <button>All</button>
                         <button>Playlists</button>
@@ -58,21 +61,50 @@ export const SearchResult = () => {
                         <button>Artists</button>
                         <button>Genres</button>
                     </div>
+                    <h2 className="top-res">Top result</h2>
                     <div className="top-result-container">
-                        <h2>Top result</h2>
-                        <img style={{width: '80px', height: '80px'}} src="../../public/img/Rap.PNG" />
+                        <img style={{ width: '80px', height: '80px' }} src="../../public/img/Rap.PNG" />
                         <h1>Hip-Hop</h1>
                         <span>Genre</span>
                     </div>
+                    <h2 className="songs">Songs</h2>
                     <SongList
                         songs={songs}
                         onAddToStation={onAddToLikedSongs}
                         onMoreOptions={onMoreOptions}
                         isSongSavedAtStation={isSongSavedAtLikedSongs}
                         type="search" />
-                </section>
+                </section> */}
+                    <div className="btns-container">
+                        <button>All</button>
+                        <button>Playlists</button>
+                        <button>Songs</button>
+                        <button>Artists</button>
+                        <button>Genres</button>
+                    </div>
+                    <section className="search-result-container1">
+                        <section className="top-result1 inner-grid">
+                            <h2 className="top-res">Top result</h2>
+                            <div className="top-result-container1">
+                                <img src="../../public/img/Rap.PNG" />
+                                <h3>Hip-Hop</h3>
+                                <span>Genre</span>
+                            </div>
+                        </section>
+                        <section className="songs-result1 inner-grid">
+                            <h2 className="songs">Songs</h2>
+                            <SongList
+                                songs={songs}
+                                onAddToStation={onAddToLikedSongs}
+                                onMoreOptions={onMoreOptions}
+                                isSongSavedAtStation={isSongSavedAtLikedSongs}
+                                type="search" />
+                        </section>
+                    </section>
+
+                </>
             )}
-            <h1>Suggestions</h1>
+            {!params.userInput && <h1>Suggestions</h1>}
         </div>
     );
 };
