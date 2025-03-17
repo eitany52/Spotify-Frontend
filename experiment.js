225259356de7548a29674993317ef7373a2af911
eitany52 <65953226+eitany52@users.noreply.github.com>
2025-03-17 11:55:59 +0200

    Delete experiment.js

--- experiment.js
+++
@@ -1,5 +0,0 @@
-// import fs from 'fs'
-
-// const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
-// let songs = loadJSON('./data/temp-songs.json')
-// fs.writeFileSync('./data/final-songs.json', JSON.stringify(songs, null, 4))
